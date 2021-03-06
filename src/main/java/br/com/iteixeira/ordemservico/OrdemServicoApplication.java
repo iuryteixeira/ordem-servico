fec7a0bf1d50f3dd19bb328792e211920205f8c2
Iury Teixeira <franciury@gmail.com>
2021-03-05 22:03:41 -0300

    Aula 1 - Criando o projeto Spring Boot

---
+++ src/main/java/br/com/iteixeira/ordemservico/OrdemServicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.iteixeira.ordemservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iteixeira/ordemservico/OrdemServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iteixeira.ordemservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
